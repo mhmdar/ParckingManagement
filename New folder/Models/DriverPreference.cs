4c0913a48851956b0308df2f48344d234a90bbe0
mhmdar <24410407+mhmdar@users.noreply.github.com>
2018-05-19 02:34:16 +0300

    adding models

---
+++ New folder/Models/DriverPreference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParkingLotAtomic.Models
+{
+    public class DriverPreference
+    {
+        public string Language { get; set; }
+        public string Currency { get; set; }
+        public string Name { get; set; }
+        public String CarId { get; set; }
+
+    }
+}
